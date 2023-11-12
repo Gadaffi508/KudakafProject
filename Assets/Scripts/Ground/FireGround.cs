e518d043f2e7d32ecd5fbc9ffa19ae5f492769e6
Yusuf <yusuf_zeki1234@hotmail.com>
2023-11-12 17:44:05 +0300

    Ground fire added

--- Assets/Scripts/Ground/FireGround.cs
+++ Assets/Scripts/Ground/FireGround.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -5,4 +6,20 @@ using UnityEngine;
 public class FireGround : MonoBehaviour
 {
     public int FireOfDamageCount;
+    public GameObject[] FireObj;
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.gameObject.CompareTag("FireBullet"))
+        {
+            FireOfDamageCount++;
+            if (FireOfDamageCount == 5)
+            {
+                foreach (GameObject fireObj in FireObj)
+                {
+                    fireObj.SetActive(true);
+                }
+            }
+        }
+    }
 }
